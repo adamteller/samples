5b2c3a246c125fc12471721690de2b91a6e6af8d
Adam Teller <34755516+adamteller@users.noreply.github.com>
2020-10-11 11:13:37 -0400

    Update consumption.sh

--- bash/consumption.sh
+++ bash/consumption.sh
@@ -2,12 +2,12 @@
 # This script is based on command below (thanks Jason Napsky)
 # du -hs * | sort -rh | head -9
 #
-# ARG 1, will be a string that 'LOOKS LIKE' a negative number for depth arg.
+# ARG 1 is how many lines you want in output.
 DISPLAYCOUNT="${1:-9}"
 
-# Validate that if we got a parameter passed in, that it is an integer.
-# Accepted answer "borrowed" from
+# Validate that DISPLAYCOUNT is an integer.  Accepted answer "borrowed" from
 # https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
+
 # Below asserts that "re" is 'NOT' a series of one ore more digits.
 re='^[0-9]+$'
 if ! [[ $DISPLAYCOUNT =~ $re ]] ; then
