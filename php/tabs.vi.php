c46ffd31a4c5f23e474ff62695494da0ea266a7d
adamteller <adam.teller@gmail.com>
2020-10-31 17:40:54 -0400

    Saved in sublime, tab stays??

--- php/tabs.vi.php
+++ php/tabs.vi.php
@@ -1,8 +1,10 @@
 <?php
-	//maybe, due to vi, this file will actually save wtabs.
-	function do_things($things) {
-		printf('My purpose is to do %s', $things);
-	}
+    //maybe, due to vi, this file will actually save wtabs.
+    function do_things($things) {
+        printf('My purpose is to do %s', $things);
+    }
 
-	do_things("mow the lawn");
-
+    do_things("mow the lawn");
+	echo "Things are done, man" .PHP_EOL;
+    //done.
+    
