ac3e7f99fc816cc6187eb3a0d9d7bad9cbd1f2ed
adamteller <adam.teller@gmail.com>
2020-10-31 17:16:11 -0400

    Trying to add code file with tabs. Does it stay?

---
+++ php/super-tab-checker.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+    * Trying out tabulations.
+    * We want to see what Sublime Text does this this.
+    * Sublime should (for this file) honor the tabs, 
+    * and not try any monkey-business of converting (until asked)
+    */
+    $noun = 'Child';
+
+    printf('Check on the %s', $noun);
